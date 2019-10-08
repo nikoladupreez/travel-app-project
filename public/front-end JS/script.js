6adad1c3ba8d25c011a53cde3b9e8f41d41855cd
Nikola du Preez <nikola_dupreez@hotmail.com>
2019-10-08 12:31:21 +0200

    hbs index and profile

---
+++ public/front-end JS/script.js
@@ -0,0 +1,35 @@
+//navbar, dropdown haburger:
+//https://materializecss.com/navbar.html
+document.addEventListener('ready', () => {
+    let hamburgerElement = document.getElementById('hamburger');
+    hamburgerElement.dropdown();
+})
+
+//slideshow-dots
+//https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+};
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("country");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += "active";
+};
