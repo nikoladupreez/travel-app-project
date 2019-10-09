4a818479346478cc178fe39d45f7e268f6220205
Rosirys quezada <rosirysquezada@gmail.com>
2019-10-09 15:45:09 +0200

    random

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const User = require("../../models/user");
-const transporter = require("../../mailer/mailer");
+const User = require("../models/user");
+const transporter = require("../mailer/mailer");
 var createError = require('http-errors');
 var jwt = require('jsonwebtoken');
-
-
-//sign up
-router.get("/signup", (req,res)=> {
-    res.render("auth/signup");
-})
 
 router.post("/signup", (req,res)=> {
     User.findOne({$or: [{username: req.body.username, email: req.body.email}]})
@@ -28,7 +22,7 @@ router.post("/signup", (req,res)=> {
                         })
                         .then((user)=> {
                             transporter.sendMail({
-                                from: '"Fred Foo 👻" <IronhackDemo2@gmail.com>', // sender address
+                                from: '"OnTrack" <Ontrack-ironhack@gmail.com>', // sender address
                                 to: user.email, // list of receivers
                                 subject: 'Welcome to OnTrack', // Subject line
                                 text: 'Welcome!', // plain text body
@@ -50,21 +44,88 @@ router.post("/signup", (req,res)=> {
         })
     })   
 
-//login
+router.get("/signup", (req,res)=> {
+    res.render("auth/signup");
+})
+router.post("/login", (req,res)=> {
+    User.findOne({username: req.body.username})
+        .then((user)=> {
+            if(!user) res.json({loggedIn: false}) // this is different
+            else {
+                bcrypt.compare(req.body.password, user.password, function(err, equal) {
+                    if(err) res.send(err);
+                    else if(!equal) res.json({loggedIn: false}); // this is different
+                    else {
+                        req.session.user = user;
+                        res.json({loggedIn: true}); // this is different
+                    }
+                });
+            }
+        })
+        .catch(err=> {
+            res.send("error error", err);
+        })
+})
+
 router.get("/login", (req,res)=> {
     res.render("auth/login");
 })
 
-//reset
-router.get("/get-reset-link", (req,res)=> {
-    res.render("auth/reset-part1");
+router.get("/logout", (req, res)=> {
+    req.session.destroy();
+    res.redirect("/");
 })
 
-router.get("/reset", (req,res)=> {
-    res.render("auth/reset-part2");
+router.post("/email-availability", (req,res)=> {
+    User.findOne({email: req.body.email})
+        .then((user)=> {
+            if(user)res.json({available: false})
+            else res.json({available: true})
+        })
 })
 
-//logout
+router.get("/send-reset", (req,res)=> {
+    res.render("auth/send-reset")
+})
 
+router.post("/send-reset", (req,res)=> {
+    jwt.sign({email: req.body.email}, process.env.jwtSecret, { expiresIn: 60 * 60 }, function(err, token){
+        transporter.sendMail({
+            from: '"OnTrack" <Ontrack-ironhack@gmail.com>', // sender address
+            to: req.body.email, // list of receivers
+            subject: 'Reset your password ✔', // Subject line
+            text: 'Reset password', // plain text body
+            html: `<b>Password reset for OnTrack: <a href="http://localhost:3000/auth/reset-password?token=${token}">Reset your password</a></b>` // html body
+        })
+        .then((result)=> {
+            res.send("Email send")
+        })
+        .catch((err)=> {
+            res.next(createError(400))
+        })
+    })
+})
+
+router.get("/reset-password", (req,res)=> {
+    res.render("auth/reset-password", {token: req.query.token})
+})
+
+router.post("/reset-password", (req,res)=> {
+    jwt.verify(req.body.token, process.env.jwtSecret, function(err, token){
+        if(err) res.send(err)
+        bcrypt.hash(req.body.password, 10, function(err, hash){
+            if(err) res.send(err)
+            else {
+                User.findOneAndUpdate({email: token.email}, {password: hash})
+                .then((result)=> {
+                    res.redirect("/auth/login")
+                })
+                .catch((err)=> {
+                    res.send(err)
+                })
+            }
+        })
+    })
+})
 
 module.exports = router;
