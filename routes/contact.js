a3f0f700055b7ae8fa283c86f4a0cb95e0af97e5
Rosirys quezada <rosirysquezada@gmail.com>
2019-10-13 22:19:07 +0200

    contact is working

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var app = express();
+const transporter = require("../mailer/mailer");
 
 /* GET contact page. */
 app.get('/', function(req, res, next) {
   res.render('contact');
 });
+
+app.post('/', function (req, res, next){
+  debugger
+  transporter.sendMail({
+    from: '"OnTrack" <Ontrack-ironhack@gmail.com>', // sender address
+    to: req.body.email, // list of receivers
+    subject: 'Contact', // Subject line
+    text: 'Hello!', // plain text body
+    html: `<b>Hello, ${req.body.name}, thank you for contacting us.</b>` // html body
+})
+.then((info)=> {
+  transporter.sendMail({
+    from: '"OnTrack" <Ontrack-ironhack@gmail.com>', // sender address
+    to: '"OnTrack" <Ontrack-ironhack@gmail.com>', // list of receivers
+    subject: `Contact from ${req.body.name}`, // Subject line
+    text: 'Hello!', // plain text body
+    html: `<b>Customer ${req.body.name} has the following concern: <br> ${req.body.note}</b>` // html body
+  })
+  .then((info)=>{
+    debugger;
+    res.redirect("/");
+  })
+  .catch((err)=> {
+    res.send(err.message);
+  })
+})
+.catch((err)=> {
+    res.send(err.message);
+
+})
+})
 
 module.exports = app;
